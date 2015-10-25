ad884514068000fb9492fe79299482b0afb09f6c
AK331 <alexa.klimchak@axtria.com>
2015-10-25 13:39:05 -0400

    added comment 2
    test

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
+## Write a short comment describing this function
 ## Write a short comment describing this function
 ## Write a short comment describing this function
 
